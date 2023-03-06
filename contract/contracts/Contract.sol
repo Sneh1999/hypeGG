39776118dc10ee220febd30b1193fc08f5b23a4c
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-03-06 11:53:17 +0800

    Finished Contract

--- contract/contracts/Contract.sol
+++ contract/contracts/Contract.sol
@@ -1,52 +1,25 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@thirdweb-dev/contracts/base/ERC1155Drop.sol";
 
-contract SoulBoundToken is ERC721, Ownable, ERC721URIStorage {
-    using Counters for Counters.Counter;
+contract HypeGG is ERC1155Drop {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps,
+        address _primarySaleRecipient
+    ) ERC1155Drop(_name, _symbol, msg.sender, 0, msg.sender) {}
 
-    Counters.Counter private _tokenIdCounter;
-
-    constructor() ERC721("SoulBoundToken", "SBT") {}
-
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-    }
-
-    function burn(uint256 tokenId) external {
-        require(
-            ownerOf(tokenId) == msg.sender,
-            "Only the owner of the token can burn it."
-        );
-        _burn(tokenId);
-    }
-
-    function _beforeTokenTransfer(
+    function safeTransferFrom(
         address from,
         address to,
-        uint256 tokenId
-    ) internal pure override {
-        require(
-            from == address(0) || to == address(0),
-            "This a Soulbound token. It cannot be transferred. It can only be burned by the token owner."
-        );
-    }
-
-    function _burn(
-        uint256 tokenId
-    ) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public override {
+        require(from == address(0), "SBTs Can't be transferred");
+        super.safeTransferFrom(from, to, id, amount, data);
     }
 }
