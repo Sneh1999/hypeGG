200b1dffe6105880139f034097d68f77e4fe47be
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-03-07 22:19:21 +0800

    updated something

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaDiscord } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="mt-40 flex items-center justify-between border-t-2 px-4 py-2">
+    <div className="mt-40 flex items-center justify-between border-t-2 px-4 py-2 sticky top-full">
       <div className="flex items-center justify-center gap-4">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
