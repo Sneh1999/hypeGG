2f0e6bc4e05f60ee499fe773759a051fe11065fb
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-03-07 11:51:18 +0800

    Added Fantom support with thirdweb SDK

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,26 @@ import type { AppProps } from "next/app";
 import { Nunito } from "@next/font/google";
 
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { Chain, configureChains, createClient, WagmiConfig, useSigner, } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc"
 import { fantomTestnet } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChainId, ThirdwebSDKProvider } from "@thirdweb-dev/react";
 
 const { chains, provider } = configureChains(
   [fantomTestnet],
-  [publicProvider()]
+  [
+    jsonRpcProvider({
+      rpc: (chain) =>
+        chain.id === ChainId.FantomTestnet
+          ? {
+              http: `https://fantom-testnet.rpc.thirdweb.com`,
+            }
+          : null,
+    }),
+  ]
 );
 
 const { connectors } = getDefaultWallets({
@@ -28,24 +38,47 @@ const wagmiClient = createClient({
   provider,
 });
 
+function ThirdwebProvider({
+  wagmiClient,
+  children,
+}: {
+  wagmiClient: any;
+  children: React.ReactNode;
+}) {
+  const { data: signer } = useSigner();
+
+  return (
+    <ThirdwebSDKProvider
+      activeChain={ChainId.Mumbai}
+      signer={signer as any}
+      queryClient={wagmiClient.queryClient as any}
+    >
+      {children}
+    </ThirdwebSDKProvider>
+  );
+}
+
 const nunito = Nunito({
   subsets: ["latin"],
   // weight: ["300", "400", "700", "900"]
 });
 
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={nunito.className}>
-      <ThirdwebProvider activeChain="fantom-testnet">
       <WagmiConfig client={wagmiClient}>
         <RainbowKitProvider chains={chains}>
+      <ThirdwebProvider
+      wagmiClient={wagmiClient}
+      >
           <Layout>
             <ToastContainer />
             <Component {...pageProps} />
           </Layout>
+      </ThirdwebProvider>
         </RainbowKitProvider>
       </WagmiConfig>
-      </ThirdwebProvider>
     </main>
   );
 }
