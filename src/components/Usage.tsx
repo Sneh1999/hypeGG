5d21ba1835f92e6a7631a5a5ef2793f7946efdad
Sneh1999 <snehkoul1999@gmail.com>
2023-03-07 23:31:17 -0600

    snapshot diff

--- src/components/Usage.tsx
+++ src/components/Usage.tsx
@@ -10,16 +10,15 @@ const Usage = () => {
     <div>
       <div className="flex items-center justify-center font-extrabold">
         <Image src={headingImage} width={70} height={50} alt="heading Image" />
-        <h1 className="text-[#7165FF] text-3xl py-10">How it Works</h1>
+        <h1 className="py-10 text-3xl text-[#7165FF]">How it Works</h1>
       </div>
-      <div className="flex items-col items-center justify-start px-2 sm:pl-20 py-10">
-        <div className="flex flex-col items-center sm:items-start sm:flex-row bg-[#7367FF] py-10 sm:w-7/12 md:w-6/12 rounded-3xl sm:px-4 border-l-8 border-[#FF6581]">
+      <div className="flex items-center justify-start px-2 py-10 sm:pl-20">
+        <div className="flex flex-col items-center rounded-3xl border-l-8 border-[#FF6581] bg-[#7367FF] py-10 sm:w-7/12 sm:flex-row sm:items-start sm:px-4 md:w-6/12">
           <div className="w-11/12 text-white">
-            <h1 className="text-xl sm:text-2xl font-bold py-3">Send HypeGG</h1>
+            <h1 className="py-3 text-xl font-bold sm:text-2xl">Send HypeGG</h1>
             <p className="pb-7">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
-              culpa numquam consequuntur ipsum aut dicta ad suscipit, quaerat ea
-              nostrum
+              Create a collection of Soulbound NFTs and upload an image that
+              represents the soulbound NFT
             </p>
           </div>
           <div>
@@ -27,16 +26,15 @@ const Usage = () => {
           </div>
         </div>
       </div>
-      <div className="flex items-col items-center justify-end px-2 sm:pr-20 py-10">
-        <div className="flex flex-col items-center sm:items-start sm:flex-row bg-[#FF6581] py-10 sm:w-7/12 md:w-6/12 rounded-3xl sm:px-4 border-r-8 border-[#7367FF]">
+      <div className="flex items-center justify-end px-2 py-10 sm:pr-20">
+        <div className="flex flex-col items-center rounded-3xl border-r-8 border-[#7367FF] bg-[#FF6581] py-10 sm:w-7/12 sm:flex-row sm:items-start sm:px-4 md:w-6/12">
           <div className="w-11/12 text-white">
-            <h1 className="text-xl sm:text-2xl font-bold py-3">
+            <h1 className="py-3 text-xl font-bold sm:text-2xl">
               Distribute HypeGG
             </h1>
             <p className="pb-7">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
-              culpa numquam consequuntur ipsum aut dicta ad suscipit, quaerat ea
-              nostrum
+              Upload a CSV containing all the addresses to whom you would like
+              to distribute the NFTs
             </p>
           </div>
           <div>
@@ -49,16 +47,14 @@ const Usage = () => {
           </div>
         </div>
       </div>
-      <div className="flex items-col items-center justify-start px-2 sm:pl-20 py-10">
-        <div className="flex flex-col items-center sm:items-start sm:flex-row bg-[#7367FF] py-10 sm:w-7/12 md:w-6/12 rounded-3xl sm:px-4 border-l-8 border-[#FF6581]">
+      <div className="flex items-center justify-start px-2 py-10 sm:pl-20">
+        <div className="flex flex-col items-center rounded-3xl border-l-8 border-[#FF6581] bg-[#7367FF] py-10 sm:w-7/12 sm:flex-row sm:items-start sm:px-4 md:w-6/12">
           <div className="w-11/12 text-white">
-            <h1 className="text-xl sm:text-2xl font-bold py-3">
+            <h1 className="py-3 text-xl font-bold sm:text-2xl">
               Review HypeGG
             </h1>
             <p className="pb-7">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
-              culpa numquam consequuntur ipsum aut dicta ad suscipit, quaerat ea
-              nostrum
+              Review all the details and Lazy mint the collection
             </p>
           </div>
           <div>
