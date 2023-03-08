b0803b31752f1d4affdf973c354dc2b24c6ab528
Sneh1999 <snehkoul1999@gmail.com>
2023-03-08 00:16:35 -0600

    fix max claimablee

--- src/components/ReviewHypeForm.tsx
+++ src/components/ReviewHypeForm.tsx
@@ -15,7 +15,7 @@ const ReviewHypeForm = () => {
   const createHypeStore = useCreateHypeStore();
   const { contract } = useContract(contractAddress);
   const { data } = useContractRead(contract, "nextTokenIdToMint");
-  const tokenId = data?.toNumber() - 1;
+  const tokenId = 0;
   console.log(tokenId);
   console.log(contract);
   const { mutateAsync: lazyMint } = useLazyMint(contract);
@@ -26,19 +26,20 @@ const ReviewHypeForm = () => {
 
   const handleMintButton = async (): Promise<void> => {
     try {
-      await lazyMint({
-        metadatas: [
-          {
-            name: createHypeStore.collection,
-            description: createHypeStore.community,
-            image: createHypeStore.image,
-          },
-        ],
-      });
+      // await lazyMint({
+      //   metadatas: [
+      //     {
+      //       name: createHypeStore.collection,
+      //       description: createHypeStore.community,
+      //       image: createHypeStore.image,
+      //     },
+      //   ],
+      // });
       const addresses = [];
       for (let i = 1; i < createHypeStore.addresses.length; i++) {
         addresses.push({
           address: createHypeStore.addresses[i],
+          maxClaimable: 1,
         });
       }
       console.log(addresses);
@@ -50,7 +51,7 @@ const ReviewHypeForm = () => {
             },
             currencyAddress: NATIVE_TOKEN_ADDRESS,
             price: 0,
-            maxClaimablePerWallet: 1,
+            maxClaimablePerWallet: 0,
             maxClaimableSupply: 1000,
             startTime: new Date(),
             waitInSeconds: 60 * 60 * 24 * 7,
